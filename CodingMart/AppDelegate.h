3c72b352a99c28ad351a967f2b8fe1eb595093f4
wangyuanchuang <wangyuanchuang@qq.com>
2015-10-08 14:32:59 +0800

    Initial Commit

---
+++ CodingMart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodingMart
+//
+//  Created by Ease on 15/10/8.
+//  Copyright © 2015年 net.coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
