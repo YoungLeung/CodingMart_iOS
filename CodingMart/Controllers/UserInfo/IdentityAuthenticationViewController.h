fded61ae056cac4f783ff5893e8cc03b562719a8
HuiYang <xiaoyangh@foxmail.com>
2015-11-17 21:37:39 +0800

    mark

---
+++ CodingMart/Controllers/UserInfo/IdentityAuthenticationViewController.h
@@ -0,0 +1,14 @@
+//
+//  IdentityAuthenticationViewController.h
+//  CodingMart
+//
+//  Created by HuiYang on 15/11/17.
+//  Copyright © 2015年 net.coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseTableViewController.h"
+
+@interface IdentityAuthenticationViewController : BaseTableViewController
+
+@end
---
+++ CodingMart/Controllers/UserInfo/IdentityAuthenticationViewController.m
@@ -0,0 +1,47 @@
+//
+//  IdentityAuthenticationViewController.m
+//  CodingMart
+//
+//  Created by HuiYang on 15/11/17.
+//  Copyright © 2015年 net.coding. All rights reserved.
+//
+
+#import "IdentityAuthenticationViewController.h"
+
+@interface IdentityAuthenticationViewController ()
+
+@end
+
+@implementation IdentityAuthenticationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title=@"身份认证";
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+//    [super tableView:tableView willDisplayCell:cell forRowAtIndexPath:indexPath];
+//    if (indexPath.row == 2) {
+//        cell.separatorInset = UIEdgeInsetsMake(0, kScreen_Width, 0, 0);//隐藏掉它
+//    }else{
+//        cell.separatorInset = UIEdgeInsetsMake(0, 15, 0, 15);
+//    }
+//}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
