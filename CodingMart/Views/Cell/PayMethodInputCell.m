119034853f44ee47f758b79ff882bfcce5719919
王 原闯 <wangyuanchuang@qq.com>
2016-02-03 17:40:41 +0800

    金额输入。。规则

--- CodingMart/Views/Cell/PayMethodInputCell.m
+++ CodingMart/Views/Cell/PayMethodInputCell.m
@@ -8,10 +8,15 @@
 
 #import "PayMethodInputCell.h"
 
+@interface PayMethodInputCell ()<UITextFieldDelegate>
+
+@end
+
 @implementation PayMethodInputCell
 
 - (void)awakeFromNib {
     // Initialization code
+    _textF.delegate = self;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -21,5 +26,30 @@
 }
 + (CGFloat)cellHeight{
     return 44.0;
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if (string.length <= 0) {
+        return YES;
+    }
+    
+    BOOL shouldChange = YES;
+    NSInteger textLength = textField.text.length;
+    BOOL stringIsDot = [string isEqualToString:@"."];
+    
+    NSRange dotRange = [textField.text rangeOfString:@"."];
+    if (dotRange.location == NSNotFound) {
+        if (stringIsDot) {
+            shouldChange = textLength - (range.location + range.length) <= 2;//插入小数点
+        }
+    }else{
+        if (stringIsDot) {
+            shouldChange = range.location <= dotRange.location && range.location + range.length > dotRange.location;//替换小数点
+        }else{
+            shouldChange = !(textLength - (dotRange.location + dotRange.length) >= 2 && textLength - (range.location + range.length) <= 2);//小数点后最多两位
+        }
+    }
+    return shouldChange;
 }
 @end
