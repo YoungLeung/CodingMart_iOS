44168a032946eecd1ac8cd1369f4b101cf57bf05
王 原闯 <wangyuanchuang@qq.com>
2016-08-18 15:31:46 +0800

    快速发布悬赏样式优化

--- CodingMart/Controllers/PublishReward/PublishRewardViewController.m
+++ CodingMart/Controllers/PublishReward/PublishRewardViewController.m
@@ -20,14 +20,15 @@
 #import "LoginViewController.h"
 #import "PublishedRewardsViewController.h"
 #import "CountryCodeListViewController.h"
+#import "EATipView.h"
 
 @interface PublishRewardViewController ()
 @property (strong, nonatomic) Reward *rewardToBePublished;
 @property (strong, nonatomic) NSArray *typeList, *budgetList;
 @property (assign, nonatomic) NSNumber *agreementChecked;
 
-@property (weak, nonatomic) IBOutlet UILabel *typeL;
-@property (weak, nonatomic) IBOutlet UILabel *budgetL;
+@property (weak, nonatomic) IBOutlet UITextField *typeL;
+@property (weak, nonatomic) IBOutlet UITextField *budgetL;
 @property (weak, nonatomic) IBOutlet UITextField *nameF;
 @property (weak, nonatomic) IBOutlet UIPlaceHolderTextView *descriptionTextView;
 
@@ -35,7 +36,6 @@
 @property (weak, nonatomic) IBOutlet UITextField *contact_mobileF;
 @property (weak, nonatomic) IBOutlet UITextField *contact_mobile_codeF;
 @property (weak, nonatomic) IBOutlet PhoneCodeButton *codeBtn;
-@property (weak, nonatomic) IBOutlet UITextField *survey_extraF;
 
 @property (weak, nonatomic) IBOutlet UIButton *checkBtn;
 @property (weak, nonatomic) IBOutlet UITTTAttributedLabel *agreementL;
@@ -98,7 +98,6 @@
     _descriptionTextView.text = _rewardToBePublished.description_mine;
     _contact_nameF.text = _rewardToBePublished.contact_name;
     _contact_mobileF.text = _rewardToBePublished.contact_mobile;
-    _survey_extraF.text = _rewardToBePublished.survey_extra;
     
     [_nameF.rac_textSignal subscribeNext:^(NSString *newText){
         self.fd_interactivePopDisabled = newText.length > 0;
@@ -118,9 +117,6 @@
         weakSelf.rewardToBePublished.contact_mobile_code = newText;
     }];
 
-    [_survey_extraF.rac_textSignal subscribeNext:^(NSString *newText){
-        weakSelf.rewardToBePublished.survey_extra = newText;
-    }];
     [RACObserve(self, agreementChecked) subscribeNext:^(NSNumber *agreementChecked) {
         [weakSelf.checkBtn setImage:[UIImage imageNamed:(agreementChecked.boolValue? @"checkbox_checked": @"checkbox_check")] forState:UIControlStateNormal];
     }];
@@ -208,6 +204,24 @@
     }
 }
 
+- (IBAction)descriptionTipBtnClicked:(id)sender {
+    NSString *tipStr =
+@"简介 \n\
+本项目是理财产品的手机 APP 开发,旨在为⽤用户提供便利的⼿机端理财产品； 产品主要功能为购买、交易理财产品。\n\
+\n\
+功能需求\n\
+适配 iPhone 4s 及以上 iPhone 设备， iOS7.0 及以上版本。功能包含： 启动引导页、账户功能、交易功能、收益计算器、银行卡绑定、消息通知与推送\n\
+\n\
+APP 架构\n\
+APP 主要有“热门推荐”、“理财超市”、“我的资产”、“更多”等页面：\n\
+[热门推荐] - 包含消息中心、轮播广告位、产品推荐等功能；\n\
+[理财超市] - 包含由分段控制切换的三个页面，三个页面为三种不同类型产品的列表， 点击进入产品详情页；\n\
+[我的资产] - 包含收益展示、图标展示资产明细、交易记录、银行卡管理、取现等功能。";
+    EATipView *tipV = [EATipView instancetypeWithTitle:@"项目描述范例" tipStr:tipStr];
+    [tipV showInView:kKeyWindow];
+}
+
+
 - (IBAction)checkBtnClicked:(id)sender {
     self.agreementChecked = @(!self.agreementChecked.boolValue);
 }
@@ -265,16 +279,37 @@
 
 #pragma mark Table
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
-    return [UIView new];
+    if (section == 1) {
+        UIView *headerV = [UIView new];
+        UILabel *titleL = ({
+            UILabel *label = [UILabel new];
+            label.font = [UIFont systemFontOfSize:14];
+            label.textColor = [UIColor colorWithHexString:@"0x999999"];
+            label;
+        });
+        [headerV addSubview:titleL];
+        [titleL mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(headerV).offset(15);
+            make.centerY.equalTo(headerV);
+        }];
+        titleL.text = @"联系信息";
+        return headerV;
+    }else{
+        return [UIView new];
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    return section == 0? 0: 10;
+    return section == 0? 15: 44;
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
     [super tableView:tableView willDisplayCell:cell forRowAtIndexPath:indexPath];
-    cell.separatorInset = UIEdgeInsetsMake(0, (indexPath.section == 0 && indexPath.row ==0)? 15: kScreen_Width, 0, 0);
+    if (indexPath.row == 0) {
+        cell.separatorInset = UIEdgeInsetsMake(0, 15, 0, 15);
+    }else{
+        cell.separatorInset = UIEdgeInsetsMake(0, kScreen_Width, 0, 0);
+    }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -282,7 +317,7 @@
     if (indexPath.section == 0) {
         NSArray *list;
         NSInteger curRow;
-        if (indexPath.row == 0) {
+        if (indexPath.row == 1) {
             list = _typeList;
             if (_rewardToBePublished.type) {
                 NSString *key = [[NSObject rewardTypeLongDict] findKeyFromStrValue:_rewardToBePublished.type.stringValue];
@@ -300,7 +335,7 @@
         __weak typeof(self) weakSelf = self;
         [ActionSheetStringPicker showPickerWithTitle:nil rows:@[list] initialSelection:@[@(curRow)] doneBlock:^(ActionSheetStringPicker *picker, NSArray *selectedIndex, NSArray *selectedValue) {
             NSNumber *selectedRow = selectedIndex.firstObject;
-            if (indexPath.row == 0) {
+            if (indexPath.row == 1) {
                 NSString *value = [[NSObject rewardTypeLongDict] objectForKey:list[selectedRow.integerValue]];
                 weakSelf.rewardToBePublished.type = @(value.integerValue);
             }else{
