73fcd846e84d9cb9cef5d57960e6074aa3e7b70a
王 原闯 <wangyuanchuang@qq.com>
2016-08-24 16:59:47 +0800

    异常崩溃 - bug

---
+++ CodingMart/Util/Category/UIWebView+Common.h
@@ -0,0 +1,13 @@
+//
+//  UIWebView+Common.h
+//  CodingMart
+//
+//  Created by Ease on 16/8/24.
+//  Copyright © 2016年 net.coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (Common)
+
+@end
---
+++ CodingMart/Util/Category/UIWebView+Common.m
@@ -0,0 +1,26 @@
+//
+//  UIWebView+Common.m
+//  CodingMart
+//
+//  Created by Ease on 16/8/24.
+//  Copyright © 2016年 net.coding. All rights reserved.
+//
+
+#import "UIWebView+Common.h"
+
+@implementation UIWebView (Common)
+
++ (void)load{
+    Class class = NSClassFromString(@"WebActionDisablingCALayerDelegate");
+    
+    SEL willBeRemoved = NSSelectorFromString(@"willBeRemoved");
+    SEL removeFromSuperview = NSSelectorFromString(@"removeFromSuperview");
+    
+    class_addMethod(class, willBeRemoved, (IMP)emptyMethod, nil);
+    class_addMethod(class, removeFromSuperview, (IMP)emptyMethod, nil);
+}
+
+void emptyMethod(id self, SEL _cmd){
+}
+
+@end
