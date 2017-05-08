dfe61be6bb090acd66c93e55dd1498149a1343fe
Easeeeeeeeee <easeeeeeeeee@ease-mbp>
2017-05-08 15:56:53 +0800

    发送验证码的按钮，样式改了

--- CodingMart/Views/PhoneCodeButton.m
+++ CodingMart/Views/PhoneCodeButton.m
@@ -11,6 +11,7 @@
 @interface PhoneCodeButton ()
 @property (nonatomic, strong, readwrite) NSTimer *timer;
 @property (assign, nonatomic) NSTimeInterval durationToValidity;
+@property (strong, nonatomic) UIView *leftLineV;
 @end
 
 @implementation PhoneCodeButton
@@ -33,8 +34,19 @@
     [super setEnabled:enabled];
     UIColor *foreColor = [UIColor colorWithHexString:enabled? @"0x4289DB": @"0xCCCCCC"];
     if (!_hasBG) {
-        [self doBorderWidth:1.0 color:foreColor cornerRadius:2.0];
+        self.titleLabel.font = [UIFont systemFontOfSize:15];
         [self setTitleColor:foreColor forState:UIControlStateNormal];
+        if (!_leftLineV) {
+            _leftLineV = [UIView new];
+            _leftLineV.backgroundColor = kColorTextLightDD;
+            [self addSubview:_leftLineV];
+            [_leftLineV mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.centerY.equalTo(self);
+                make.size.mas_equalTo(CGSizeMake(1.0, 20));
+                make.left.equalTo(self).offset(-8);
+            }];
+        }
+//        [self doBorderWidth:1.0 color:foreColor cornerRadius:2.0];
     }else{
         self.backgroundColor = foreColor;
     }
